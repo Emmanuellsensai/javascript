286dc9f5eed5e6a6d97775713b05437aa59e9796
emmanuellsensai <emmanuelusang60@gmail.com>
2026-09-16 16:54:41 +0100

    Day 2 - part 1 and 2 done

--- DAY 2/fizzbuzz.js
+++ DAY 2/fizzbuzz.js
@@ -1,7 +1,9 @@
 let number = "";
 
-if(number%3==0){
+if (number % 3 == 0) {
     console.log("fizz")
-}else if(number%2==0){
+} else if (number % 5 == 0) {
     console.log("buzz")
+} else if (number % 3 && number%5){
+    console.log("fizzbuzz")
 }
